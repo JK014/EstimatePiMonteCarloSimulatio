2e6eaec2449d622b4df38100abd342d3acf8756d
BuildTools <unconfigured@null.spigotmc.org>
2020-06-29 11:54:39 +0200

    Implemented corss building for Mac, Linux, Windows

--- src/main/java/org/jkweb/App.java
+++ src/main/java/org/jkweb/App.java
@@ -7,17 +7,12 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 /**
---
+++ src/main/java/org/jkweb/AppLauncher.java
@@ -0,0 +1,7 @@
+package org.jkweb;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
